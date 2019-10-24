8a4921ef8f819b29cfdcd35192ef78fcfd1b6ff7
sreekanthreddyv <sreekanthreddy.v@live.com>
2019-10-24 06:37:36 +0530

    New Begin

--- hello.py
+++ hello.py
@@ -1,11 +1,26 @@
-import pandas as pd
+# import pandas as pd
 
-df = pd.read_csv("gapminder.tsv", sep='\t')
+# df = pd.read_csv("gapminder.tsv", sep='\t')
 
-# print(df.head())
+# # print(df.head())
 
-print(df.shape)
+# print(df.shape)
 
-print(df.columns)
+# print(df.columns)
 
-print(df.dtypes)
+# print(df.dtypes)
+
+# # print(df.info())
+
+# subset = df[['country', 'continent', 'year']]
+
+# # print(subset.head())
+
+
+# print(df.loc[4])
+
+# print("iloc")
+# print(df.iloc[:, [0,1,2]].head(n=1))
+
+# print(df.iloc[:, :3].head())
+
