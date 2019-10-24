29762d79dd0fc9835afc0f30989cf433dcdf7bef
sreekanthreddyv <sreekanthreddy.v@live.com>
2019-10-24 06:50:24 +0530

    Practice Git

---
+++ iDic.py
@@ -0,0 +1,6 @@
+data = {'a':1, 'b': 2, 'c':3}
+
+# print(data)
+
+def ha():
+	print("Hacker Online")
---
+++ test_iDIc.py
@@ -0,0 +1,8 @@
+from importlib import import_module
+
+b = import_module('iDic')
+
+print(b.data)
+
+b.ha()
+
